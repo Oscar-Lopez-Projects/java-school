543f70a3fecbd1ad48ceb98e883bb822f8f6de3d
oscar-lopez-projects <d1lopeztv@gmail.com>
2020-08-18 08:37:02 -0700

    handlers working

--- schools/src/main/java/com/lambdaschool/schools/exceptions/CustomErrorDetails.java
+++ schools/src/main/java/com/lambdaschool/schools/exceptions/CustomErrorDetails.java
@@ -16,19 +16,22 @@ public class CustomErrorDetails extends DefaultErrorAttributes {
     HelperFunctions helperFunctions;
 
     @Override
-    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
-        Map<String, Object > errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+    public Map<String, Object> getErrorAttributes(
+            WebRequest webRequest,
+            boolean includeStackTrace)
+    {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+
         Map<String, Object> errorDetails = new LinkedHashMap<>();
         errorDetails.put("title", errorAttributes.get("error"));
         errorDetails.put("status", errorAttributes.get("status"));
-        errorDetails.put("details", errorAttributes.get("message"));
+        errorDetails.put("detail", errorAttributes.get("message"));
         errorDetails.put("timestamp", errorAttributes.get("timestamp"));
+        errorDetails.put("developerMessage", "path " + errorAttributes.get("path"));
 
-        errorAttributes.put("developerMessage", "path " + errorAttributes.get("path"));
-
-        errorAttributes.put("errors", helperFunctions.getConstraintViolation(this.getError(webRequest)));
-
+        errorDetails.put("errors", helperFunctions.getConstraintViolation(this.getError(webRequest)));
 
         return errorDetails;
     }
+
 }
--- schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
+++ schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.lambdaschool.schools.handlers;
 
+import com.lambdaschool.schools.models.ErrorDetail;
+import com.lambdaschool.schools.services.HelperFunctions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,8 +12,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @Autowired
+    HelperFunctions helperFunctions;
+
     public RestExceptionHandler() {
         super();
     }
@@ -20,6 +31,15 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
-        
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date());
+        errorDetail.setStatus(status.value());
+        errorDetail.setTitle("Rest Internal Exception");
+        errorDetail.setDetail(ex.getMessage());
+        errorDetail.setDeveloperMessage(ex.getClass().getName());
+
+        errorDetail.setErrors(helperFunctions.getConstraintViolation(ex));
+        return new ResponseEntity<>(errorDetail, null, status);
+
     }
 }
