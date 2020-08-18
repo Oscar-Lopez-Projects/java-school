751efdce5ea52e3be888d649e33d1b4f5e79a28b
oscar-lopez-projects <d1lopeztv@gmail.com>
2020-08-18 08:58:04 -0700

    MVP

--- schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
+++ schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.lambdaschool.schools.handlers;
 
+import com.lambdaschool.schools.exceptions.ResourceNotFoundException;
 import com.lambdaschool.schools.models.ErrorDetail;
 import com.lambdaschool.schools.services.HelperFunctions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -23,7 +25,18 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public RestExceptionHandler() {
         super();
     }
-
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle("Resource Not Found");
+        errorDetail.setDetail(rnfe.getMessage());
+        errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+        errorDetail.setErrors(helperFunctions.getConstraintViolation(rnfe));
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(
             Exception ex,
